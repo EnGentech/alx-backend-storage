89b993d43fe5adb193f0e20fea2c8014111dbbc1
EnGentech <engen.inyang@gmail.com>
2023-10-11 20:34:11 +0100

    gis

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,6 +1,7 @@
 --creating a user table if it does not exist
 
-CREATE TABLE IF NOT EXISTS `users`(
-    `id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+CREATE TABLE IF NOT EXISTS `users` (
+    `id` INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
     `email` VARCHAR(255) NOT NULL UNIQUE,
-    `name` VARCHAR(255));
+    `name` VARCHAR(255)
+);
