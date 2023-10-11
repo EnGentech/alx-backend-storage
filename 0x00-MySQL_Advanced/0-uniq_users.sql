5c372088ac39c390b551201211db15bbfc322b3e
EnGentech <engen.inyang@gmail.com>
2023-10-11 20:28:14 +0100

    created a table is not exists

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,6 +1,6 @@
--- Creates table users with variables id, email and name
-CREATE TABLE IF NOT EXISTS `users` (
-    `id` INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    `email` VARCHAR(255) NOT NULL UNIQUE,
-    `name` VARCHAR(255)
-);
+--creating a user table if it does not exist
+
+CREATE TABLE IF NOT EXISTS users(
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255));
