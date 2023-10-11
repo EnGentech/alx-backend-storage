b46627cd6af30166bff7ccc247d16405844d7558
EnGentech <engen.inyang@gmail.com>
2023-10-11 20:36:44 +0100

    j]

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,4 +1,4 @@
--- Creates table users with variables id, email and name
+--Creates table users with variables id, email and name
 CREATE TABLE IF NOT EXISTS `users` (
     `id` INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
     `email` VARCHAR(255) NOT NULL UNIQUE,
