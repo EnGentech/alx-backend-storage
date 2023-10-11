89e72bf82d3718a03afab86bd4c02495008725c9
EnGentech <engen.inyang@gmail.com>
2023-10-11 22:56:37 +0100

    fixed bugs

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,4 +1,6 @@
 -- Query database with data grouped by the sum of fans
 
-select origin, sum(fans) as nb_fans group by origin
+select origin, sum(fans) as nb_fans 
+from metal_bands
+group by origin
 order by nb_fans desc;
