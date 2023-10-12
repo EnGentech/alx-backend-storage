a6ff0f326aa51c7e27ff9fa7a0272076abeb6924
EnGentech <engen.inyang@gmail.com>
2023-10-12 11:51:20 +0100

    track email changes

---
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -0,0 +1,14 @@
+-- triger an even when theres an update on an email
+
+drop trigger if exists validate_email
+delimiter $$
+create trigger validate_email 
+before update on users for each row
+begin
+if new.email <> old.email then
+set new.valid_email = 1;
+else
+set new.valid_email = 0;
+end if;
+end $$
+
