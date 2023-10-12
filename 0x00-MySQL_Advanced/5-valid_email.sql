ceb823acf9c89b40c62cf9c57c51270e2b3784f4
EnGentech <engen.inyang@gmail.com>
2023-10-12 12:08:08 +0100

    fixed bugs

--- 0x00-MySQL_Advanced/5-valid_email.sql
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -1,11 +1,11 @@
 -- triger an even when theres an update on an email
 
-drop trigger if exists validate_email
+drop trigger if exists validate_email;
 delimiter $$
 create trigger validate_email 
 before update on users for each row
 begin
     if old.email <> new.email then
-    set new.valid_email = 0;
+        set new.valid_email = 0;
 end if;
 end $$
