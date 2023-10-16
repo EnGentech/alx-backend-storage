013cc3e21665c72ecbe85da8b9a81a9962405015
EnGentech <engen.inyang@gmail.com>
2023-10-16 23:26:02 +0100

    fixed bugs

--- 0x01-NoSQL/9-insert_school.py
+++ 0x01-NoSQL/9-insert_school.py
@@ -6,5 +6,5 @@ def insert_school(mongo_collection, **kwargs):
     """function to insert key value: data in mongo_collection"""
     for key, values in kwargs.items():
         mongo_collection.insert_one({key:values})
-
+        return mongo_collection._id
 # Coded by EnGentech
