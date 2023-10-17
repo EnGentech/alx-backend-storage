deb1d66184c288f62b90a7a1aaf522ebf12f9d75
EnGentech <engen.inyang@gmail.com>
2023-10-17 23:23:44 +0100

    zee pasker

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -1,9 +1,27 @@
 #!/usr/bin/env python3
 """update many to affect changes"""
 
+from pymongo import MongoClient
 
-def schools_by_topic(mongo_collection, topic):
-    '''A function to return an array containing a certain topic of interest'''
-    return mongo_collection.find({'topics': {'$in':[topic]}})
-    
-# Coded by EnGentech
+
+if __name__ == "__main__":
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    nginx = client.logs.nginx
+    total_doc = nginx.count_documents({})
+    get = nginx.count_documents({"method": {"$eq": "GET"}})
+    post = nginx.count_documents({"method": {"$eq": "POST"}})
+    put = nginx.count_documents({"method": {"$eq": "PUT"}})
+    patch = nginx.count_documents({"method": {"$eq": "PATCH"}})
+    delete = nginx.count_documents({"method": {"$eq": "DELETE"}})
+    both = nginx.count_documents({"$and": [
+                                          {"method": {"$eq": "GET"}},
+                                          {"path": {"$eq": "/status"}}
+                                          ]})
+    print('{} logs'.format(total_doc))
+    print("Methods:")
+    print('\tmethod GET: {}'.format(get))
+    print('\tmethod POST: {}'.format(post))
+    print('\tmethod PUT: {}'.format(put))
+    print('\tmethod PATCH: {}'.format(patch))
+    print('\tmethod DELETE: {}'.format(delete))
+    print('{} status check'.format(both))
