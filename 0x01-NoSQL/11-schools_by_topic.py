afa185d9bb736862781f4c49e3e68cc5b4da869f
EnGentech <engen.inyang@gmail.com>
2023-10-17 12:07:33 +0100

    fixed bugs

--- 0x01-NoSQL/11-schools_by_topic.py
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -4,6 +4,6 @@
 
 def schools_by_topic(mongo_collection, topic):
     '''A function to return an array containing a certain topic of interest'''
-    return mongo_collection.find({'topic': topic})
+    return mongo_collection.find({'topics': {'$in':[topic]}})
     
 # Coded by EnGentech
