c61d12a6a456c82a9f0061050b541a7b6ad8d2ca
EnGentech <engen.inyang@gmail.com>
2023-10-17 12:00:55 +0100

    try this

--- 0x01-NoSQL/11-schools_by_topic.py
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -4,6 +4,6 @@
 
 def schools_by_topic(mongo_collection, topic):
     '''A function to return an array containing a certain topic of interest'''
-    return mongo_collection.find({'topic': {'$in':[topic]}})
+    return mongo_collection.find({'topic': topic})
     
 # Coded by EnGentech
