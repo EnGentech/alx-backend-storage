8076f60b86d51ea866de8fae94ed4a5509231056
EnGentech <engen.inyang@gmail.com>
2023-10-18 18:15:18 +0100

    created a function decorator

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -6,8 +6,20 @@ its python module known as redis
 
 
 from redis import Redis
+from functools import wraps
 from uuid import uuid4
 from typing import Union, Callable, Optional
+
+
+def count_calls(method: Callable) -> Callable:
+    """A function to count the number of time the class Cache was called"""
+    key = method.__qualname__
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        self._redis.incr(key)
+        return method(self, *args, *kwargs)
+    return wrapper
+
 
 
 class Cache:
@@ -20,13 +32,14 @@ class Cache:
         self._redis = Redis()
         self._redis.flushdb()
 
+    @count_calls
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """store method"""
         key = str(uuid4())
         self._redis.set(key, data)
         return key
     
-    def get(self, key: str, fn: Optional[Callable]):
+    def get(self, key: str, fn: Optional[Callable] = None) -> str:
         """A method that returns a redit data casted to user desired type
         The Optional annotation implies that the argument is not compulsary
         to be called when calling the function.
@@ -44,7 +57,7 @@ class Cache:
         """An automated method that returns a string callable"""
         return self.get(key, str)
     
-    def get_int(self, key: int):
+    def get_int(self, key: str):
         """An automated method that returns an int callable"""
         return self.get(key, int)
     
