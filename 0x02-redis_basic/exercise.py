2b580df6c1c5df0afd3582d81b4eb49bbe6a5658
EnGentech <engen.inyang@gmail.com>
2023-10-18 18:51:44 +0100

    created another decorator for tracking input and output method calls

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -14,12 +14,26 @@ from typing import Union, Callable, Optional
 def count_calls(method: Callable) -> Callable:
     """A function to count the number of time the class Cache was called"""
     key = method.__qualname__
+
     @wraps(method)
     def wrapper(self, *args, **kwargs):
         self._redis.incr(key)
         return method(self, *args, *kwargs)
     return wrapper
 
+def call_history(method: callable) -> Callable:
+    """Keep track of input and output values from user input"""
+    key_input = f'{method.__qualname__}:inputs'
+    key_output = f'{method.__qualname__}:outputs'
+
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        self._redis.rpush(key_input, str(args))
+        stdOut = method(self, *args, **kwargs)
+        self._redis.rpush(key_output, str(args))
+        return stdOut
+    return wrapper
+        
 
 
 class Cache:
@@ -33,13 +47,14 @@ class Cache:
         self._redis.flushdb()
 
     @count_calls
+    @call_history
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """store method"""
         key = str(uuid4())
         self._redis.set(key, data)
         return key
     
-    def get(self, key: str, fn: Optional[Callable] = None) -> str:
+    def get(self, key: str, fn: Optional[Callable] = None) -> Union[str, bytes, int, float, None]:
         """A method that returns a redit data casted to user desired type
         The Optional annotation implies that the argument is not compulsary
         to be called when calling the function.
